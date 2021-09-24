3d6bf270e29626fd6ab2e7fe4bafcd77dc004976
Tim McClure <mccluret@gmail.com>
2021-09-24 14:25:57 -0400

    Add front end code

--- index.js
+++ index.js
@@ -4,20 +4,18 @@ const server = express();
 const morgan = require('morgan');
 const chalk = require('chalk');
 const cors = require('cors');
-const {PORT = 3000} = process.env;
-
+const { PORT = 3001 } = process.env;
 
 const client = require('./db/client');
 client.connect();
 
 server.use(cors());
 
-
 // logging middleware
 server.use(morgan('dev'));
 // parsing middleware
 server.use(express.json());
-server.use(express.urlencoded({extended: true}));
+server.use(express.urlencoded({ extended: true }));
 
 // Serve Docs
 const path = require('path');
@@ -32,16 +30,30 @@ server.get('/', (req, res) => {
 
 // 404 handler
 server.get('*', (req, res) => {
-  res.status(404).send({error: '404 - Not Found', message: 'No route found for the requested URL'});
+  res
+    .status(404)
+    .send({
+      error: '404 - Not Found',
+      message: 'No route found for the requested URL',
+    });
 });
 
 // error handling middleware
 server.use((error, req, res, next) => {
   console.error('SERVER ERROR: ', error);
-  if(res.statusCode < 400) res.status(500);
-  res.send({error: error.message, name: error.name, message: error.message, table: error.table});
+  if (res.statusCode < 400) res.status(500);
+  res.send({
+    error: error.message,
+    name: error.name,
+    message: error.message,
+    table: error.table,
+  });
 });
 
 server.listen(PORT, () => {
-  console.log(chalk.blueBright('Server is listening on PORT:'), chalk.yellow(PORT), chalk.blueBright('Get your routine on!'));
+  console.log(
+    chalk.blueBright('Server is listening on PORT:'),
+    chalk.yellow(PORT),
+    chalk.blueBright('Get your routine on!'),
+  );
 });
---
+++ src/components/App.js
@@ -0,0 +1,5 @@
+const App = () => {
+  return <div>I will be a cool front end some day</div>;
+};
+
+export default App;
