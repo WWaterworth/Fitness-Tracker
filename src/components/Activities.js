1ed3f69e0c46f9949dc57b9f73282c12b221df85
Warren Waterworth <warren.waterworth@gmail.com>
2022-04-14 08:57:30 -0600

    public activities showing in the activities route for unregistered users

--- src/components/Activities.js
+++ src/components/Activities.js
@@ -1,5 +1,32 @@
+import React, { useState, useEffect } from "react";
+import { callApi } from "../api";
+
 const Activities = () => {
-  return <h1>This is the activities page</h1>;
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    const getActivities = async () => {
+      const data = await callApi({
+        url: `/activities`,
+        method: "GET",
+      });
+      setActivities(data);
+    };
+    getActivities();
+  }, []);
+  return (
+    <>
+      <h1>This is the activities page</h1>
+      {activities.map((activity) => {
+        return (
+          <div key={activity.id}>
+            <h2>Activity: {activity.name}</h2>
+            <p>Description: {activity.description}</p>
+          </div>
+        );
+      })}
+    </>
+  );
 };
 
 export default Activities;
--- src/components/Routines.js
+++ src/components/Routines.js
@@ -1,21 +1,19 @@
 import React, { useState, useEffect } from "react";
-
 import { callApi } from "../api";
 
 const Routines = () => {
   const [routines, setRoutines] = useState([]);
+
   useEffect(() => {
     const getRoutines = async () => {
       const data = await callApi({
         url: `/routines`,
         method: "GET",
       });
-      console.log("data from GET routines callApi", data);
       setRoutines(data);
     };
     getRoutines();
   }, []);
-  console.log("routines", routines);
   return (
     <>
       <h1>This is the routines page</h1>
